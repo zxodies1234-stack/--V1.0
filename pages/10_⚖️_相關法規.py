7f889f6ebbe4ed9d666023182919a41324f4e013
zxodies1234-stack <zxodies1234@gmail.com>
2026-05-05 15:56:37 +0800

    Delete pages directory

--- pages/10_⚖️_相關法規.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from utils import init_sidebar
-
-st.set_page_config(layout="wide")
-curr_proj = init_sidebar() # 呼叫更新後的導航
-
-st.title("⚖️ 相關法規") # 請根據上表修改標題
-st.write(f"當前專案：{curr_proj}")
-# ... 後續功能程式碼 ...
